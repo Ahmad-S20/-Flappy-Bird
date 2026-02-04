5b1644ce316862048a6c3ba3257a0afdd071f564
Ahmad-S20 <seifahmad6363@gmail.com>
2026-02-04 18:16:33 +0200

    Initial Unity project with Bird physics

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myrigidbody2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) 
+        { 
+            myrigidbody2D.linearVelocity = Vector2.up * 10;
+        }
+
+    }
+}
