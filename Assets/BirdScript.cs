ae695e7297ea3cf13b0d41215c67ccb4c41f9695
Ahmad-S20 <seifahmad6363@gmail.com>
2026-02-07 23:08:17 +0200

    Reset Game after dying

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,19 +5,25 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myrigidbody2D;
     public float birdStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame) 
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && birdIsAlive) 
         { 
             myrigidbody2D.linearVelocity = Vector2.up * birdStrength;
         }
-
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOVerScreen();
+        birdIsAlive = false;
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOver;
 
     [ContextMenu("Increase score")]
     public void addScore()
     {
         playerScore++;
         scoreText.text = playerScore.ToString();
+    }
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOVerScreen()
+    {
+        gameOver.SetActive(true);
 
     }
+
 }
